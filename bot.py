a719f8241436383738b229397f3c624dcc71fc33
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-06-28 21:09:53 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ async def on_ready():
     
     
 @bot.command()
-async def dmall(self, ctx, msg: str):
+async def dmall(ctx, *, msg: str):
     if not "CEO" in [x.name for x in ctx.author.roles]:
         return await ctx.send("NOPE! You must have the CEO role to use this command.")
     msg = await ctx.send("Please wait, DMing everyone...")
